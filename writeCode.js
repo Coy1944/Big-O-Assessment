9c3faa952b483ba90a695fefe3dfe013c6793612
Coy1944 <Samuelchincks86@gmail.com>
2022-03-25 11:02:04 -0600

    inital commit

---
+++ writeCode.js
@@ -0,0 +1,65 @@
+// 1
+
+// Time = O(n^2) this has a nested loop. it will take longer becuase it has the nested loop and will have to times
+// Space = O(n^2)
+
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+let value = "False"
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = "True"
+      } 
+    }
+  }
+}
+
+console.log(value)
+
+// 2
+// Time = O(n) the for loop will become linear over time
+// Space = O(n)
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
+
+  console.log(hasUniqueChars("moonday"))
+
+  // 3
+  // Time = O(n) function will be linear
+  // space - O(n)
+
+  function isPangram(string){
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      if(strArr.indexOf(alphabet[i]) < 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // 4 
+  // Time = O(n)
+  // Space = O(n)
+  function findLongestWord(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+      longestWord = strSplit[i].length;
+       }
+    }
+    return longestWord;
+  }
+  findLongestWord("The quick brown fox jumped over the lazy dog");
